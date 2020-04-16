92897e246d88f94a664c926a0b2c57bc55fcb1d1
Arman Davidyan <arman.davidyan@clearroad.io>
2020-04-16 14:35:53 +0200

    perf: border control page completed

--- src/app/borders/borders.component.ts
+++ src/app/borders/borders.component.ts
@@ -1,15 +1,45 @@
 import { Component, OnInit } from '@angular/core';
 
+import * as border_control from '../data/border_control';
+
 @Component({
   selector: 'app-borders',
   templateUrl: './borders.component.html',
   styleUrls: ['./borders.component.css']
 })
 export class BordersComponent implements OnInit {
+  public isMobile: boolean;
+
+  public headers = [
+    "Country", "Foreigners Ban", "Home Quarantine", "COVID-19 Testing", "Closed Borders", "Other Measures",
+    "Exceptions", "Start", "End", "Status"
+  ];
+
+  public table = [];
 
   constructor() { }
 
   ngOnInit() {
+    this.isMobile = window.innerWidth > 991 ? false : true;
+
+    this.setTable();
   }
+
+  private setTable() {
+    for (const row of border_control.default) {
+      this.table.push({
+        "country": row.country,
+        "ban": row.ban,
+        "quarantine": row.quarantine,
+        "testing": row.testing,
+        "borders": row.closed_borders,
+        "other": row.other,
+        "except": row.exceptions,
+        "start": row.start,
+        "end": row.end,
+        "status": row.status
+      });
+    }
+  } 
 
 }
---
+++ src/app/data/border_control.ts
@@ -0,0 +1,62 @@
+export default [
+  {
+    "country": "Cambodia",
+    "ban": "Most countries",
+    "quarantine": "No",
+    "testing": "Yes",
+    "closed_borders": "No",
+    "other": "Visa suspension",
+    "exceptions": "Nationals",
+    "start": "30 March 2020",
+    "end": "",
+    "status": "Ongoing"
+  },
+  {
+    "country": "Cameroon",
+    "ban": "All countries",
+    "quarantine": "",
+    "testing": "",
+    "closed_borders": "Yes",
+    "other": "Airports closed",
+    "exceptions": "",
+    "start": "17 March 2020",
+    "end": "",
+    "status": "Ongoing"
+  },
+  {
+    "country": "Canada",
+    "ban": "All countries",
+    "quarantine": "",
+    "testing": "",
+    "closed_borders": "Yes",
+    "other": "",
+    "exceptions": "Nationals, US citizens",
+    "start": "18 March 2020",
+    "end": "",
+    "status": "Ongoing"
+  },
+  {
+    "country": "Colombia",
+    "ban": "All countries",
+    "quarantine": "Yes",
+    "testing": "",
+    "closed_borders": "No",
+    "other": "",
+    "exceptions": "",
+    "start": "16 March 2020",
+    "end": "",
+    "status": ""
+  },
+  {
+    "country": "Croatia",
+    "ban": "Most countries",
+    "quarantine": "Yes",
+    "testing": "",
+    "closed_borders": "No",
+    "other": "",
+    "exceptions": "EU citizens",
+    "start": "18 March 2020",
+    "end": "",
+    "status": "Ongoing"
+  }
+]
