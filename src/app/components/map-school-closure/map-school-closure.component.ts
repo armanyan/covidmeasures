36dba39e0f771e32cdf24085479b2e1b5e42430c
eevan7a9 <e7a9@protonmail.com>
2020-04-25 12:43:36 +0900

    added info box, click popup, & hover

--- src/app/components/map-school-closure/map-school-closure.component.ts
+++ src/app/components/map-school-closure/map-school-closure.component.ts
@@ -27,7 +27,10 @@ interface Country {
 
 
 export class MapSchoolClosureComponent implements OnInit {
+
   public map: L.map;
+  public info: L.control;
+  public legend: L.control;
   public countries: Array<Country>;
 
   constructor(
@@ -41,10 +44,20 @@ export class MapSchoolClosureComponent implements OnInit {
       this.http.get('https://covidmeasures-data.s3.amazonaws.com/school_closure.json').subscribe((res:{countries: Array<Country>}) => {
         this.countries = res.countries
         this.initMap();
+        this.addInfoBox();
+        this.addLegend();
         this.shapeService.getCountriesShapes().subscribe(country => {
+          country.features.forEach(item => {
+            const foundCountry = this.countries.find(country => country.alpha3 === item.id)
+            if (foundCountry) {
+              // we add data from apit to each country
+              item.countryData = foundCountry
+            }else{
+              item.countryData = null
+            }
+          })
           this.initStatesLayer(country);
         });
-
       // we populate the maps with markers
       // this.markerService.makeStateMarkers(this.map);
       // this.markerService.makeStateCircleMarkers(this.map);
@@ -65,10 +78,61 @@ export class MapSchoolClosureComponent implements OnInit {
         attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
       });
 
-
       tiles.addTo(this.map);  
+    }
 
-      const legend = L.control({position: 'bottomright'});
+    private addInfoBox(){
+      // we add info box
+      this.info = L.control();
+
+      this.info.onAdd = function (map) {
+          this._div = L.DomUtil.create('div', 'info'); // create a div with a class "info"
+          this.update();
+          return this._div;
+      };
+
+      const getTextColor = function(text:string) {
+        if (text) {
+          const status = text.toLowerCase()
+          let color:string;
+          switch (status as any) {
+            case "no closure":
+              color = '#28a745';
+              break;
+            case "closure":
+              color = '#e6595a';
+              break;
+            case "re-opening":
+              color = '#ee7f08';
+              break;
+            case "re-openning":
+              color = '#ee7f08';
+              break;
+            case "re-open":
+              color = '#17a2b8';
+              break;
+            default:
+              color = '#555';
+              break;
+          }
+          return color
+        }
+      };
+      // method that we will use to update the control based on feature properties passed
+      this.info.update = function (country:Country) {
+          this._div.innerHTML = 
+          '<h4>School Closure Status</h4>' +  (country ?
+          `<b>${country.name}</b><br />
+          <span style="color:${getTextColor(country.status)}">${country.status}</span>`
+          : 'Hover over a Country');
+      };
+
+      this.info.addTo(this.map);
+    }
+
+    private addLegend(){
+      // we add legends to our map
+      this.legend = L.control({position: 'bottomright'});
       const getColor = function(status) {
         return  status == "No data" ? '#e3e3e3' :
                 status == "No closure" ? '#28a745' :
@@ -78,7 +142,7 @@ export class MapSchoolClosureComponent implements OnInit {
                 status == "Re-open"  ? '#17a2b8' :
                           '#e3e3e3';
       }
-      legend.onAdd = function (map) {
+      this.legend.onAdd = function (map) {
 
           const div = L.DomUtil.create('div', 'info legend'),
               status = ['No data', 'No closure', 'Nationwide Closure', 'Partial Closure', 'Re-opening', 'Re-open'],
@@ -93,7 +157,7 @@ export class MapSchoolClosureComponent implements OnInit {
           return div;
       };
 
-      legend.addTo(this.map);
+      this.legend.addTo(this.map);
     }
 
     private initStatesLayer(item) {
@@ -103,39 +167,73 @@ export class MapSchoolClosureComponent implements OnInit {
           opacity: 0.5,
           color: 'aliceblue',
           fillOpacity: 0.8,
-          fillColor: this.getFillColor(feature)
+          fillColor: this.getFillColor(feature.countryData)
         }),
-        // onEachFeature: (feature, layer) => (
-        //   layer.bindPopup("clicked").on({
-        //     mouseover: (e) => (this.highlightFeature(e)),
-        //     mouseout: (e) => (this.resetFeature(e))
-        //   })
-        // )
+        onEachFeature: (feature, layer) => (
+          layer.bindPopup(this.clickedCountry(feature.countryData)).on({
+            mouseover: (e) => (this.highlightFeature(e)),
+            mouseout: (e) => (this.resetFeature(e))
+          })
+        )
       });
       this.map.addLayer(stateLayer);
     }
 
-    private getFillColor(item:{id:string}) {
-      const foundCountry = this.countries.find(country => country.alpha3 === item.id)
-      if (foundCountry) {
-        if(foundCountry.status.toLowerCase() == "no data") return '#e3e3e3';
+    private getFillColor(country: Country) {
+      if (country) {
+        if(country.status.toLowerCase() == "no data") return '#e3e3e3';
         if(
-          foundCountry.status.toLowerCase() == "no closure") return '#28a745';
+          country.status.toLowerCase() == "no closure") return '#28a745';
         if(
-          foundCountry.status.toLowerCase() == "closure" && 
-          foundCountry.current_coverage.toLowerCase() == "general"
+          country.status.toLowerCase() == "closure" && 
+          country.current_coverage.toLowerCase() == "general"
           ) return '#e6595a';
         if(
-          foundCountry.status.toLowerCase() == "closure" && 
-          foundCountry.current_coverage.toLowerCase() == "partial"
+          country.status.toLowerCase() == "closure" && 
+          country.current_coverage.toLowerCase() == "partial"
           ) return '#ee7f08';
         if(
-          foundCountry.status.toLowerCase() == "re-openning" || 
-          foundCountry.status.toLowerCase() == "re-opening"
+          country.status.toLowerCase() == "re-openning" || 
+          country.status.toLowerCase() == "re-opening"
           ) return '#ffeb3b';
-        if(foundCountry.status.toLowerCase() == "re-open") return '#17a2b8';
+        if(country.status.toLowerCase() == "re-open") return '#17a2b8';
         else return '#e3e3e3'
       }
-      return '#e3e3e3';
+      return '#e3e3e3'
+    }
+
+    private highlightFeature(e)  {
+      const layer = e.target;
+      layer.setStyle({
+        weight: 2,
+        opacity: 1,
+        // color: '#DFA612',
+        fillOpacity: .6,
+        // fillColor: '#FAE042',
+      });
+      this.info.update(layer.feature.countryData)
+    }
+
+    private resetFeature(e)  {
+      const layer = e.target;
+      layer.setStyle({
+        weight: 3,
+        opacity: 0.5,
+        color: 'aliceblue',
+        fillOpacity: 0.8,
+        // fillColor: this.getFillColor(id)
+      });
+      this.info.update()
+    }
+    private clickedCountry(country){
+      if (country) {
+        return `
+        <div>
+          <h6>
+            <strong>${country.name}</strong>
+          </h6>
+        </div>
+      `;
+      }
     }
 }
