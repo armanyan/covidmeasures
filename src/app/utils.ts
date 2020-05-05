556eca8d73c824ff2c97bee832c3e391d8598858
Arman Davidyan <arman.davidyan@clearroad.io>
2020-05-05 21:14:20 +0200

    perf: both deqth and infection tooltip at once

--- src/app/utils.ts
+++ src/app/utils.ts
@@ -306,11 +306,9 @@ export const createLineChart = (
     },
 
     tooltips: {
-      callbacks: {
-        label: function(tooltipItem: any) {
-          return tooltipItem.yLabel.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-        }
-      }
+      enabled: true, 
+      mode: 'label',
+      position: 'nearest'
     },
 
     scales: {
