f379bc480d008e3b771d67a1d418589cfe2469ef
eevan7a9 <e7a9@protonmail.com>
2020-05-06 17:45:33 +0900

    added menu collapse

--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -8,18 +8,19 @@ declare interface RouteInfo {
     class: string;
     soon?: boolean;
     hide?: boolean;
+    belongsTo?: string;
 }
 export const ROUTES: RouteInfo[] = [
   { path: '/country', title: 'Country Overview',  icon:'outlined_flag', class: '' },
-  { path: '/lockdown', title: 'Lockdown',  icon:'lock', class: '' },
-  { path: '/school', title: 'School Closure',  icon:'school', class: '' },
+  { path: '/lockdown', title: 'Lockdown', icon:'lock', class: '',  belongsTo: 'measures' },
+  { path: '/school', title: 'School Closure',  icon:'school', class: '',  belongsTo: 'measures' },
   { path: '/impacts', title: 'Impacts',  icon:'vertical_align_center', class: '' },
-  { path: '/covid', title: 'COVID-19 Statistics',  icon: 'table_chart', class: '' },
-  { path: '/deathrates', title: 'Death Rates',  icon:'trending_up', class: '' },
+  { path: '/covid', title: 'COVID-19',  icon: 'table_chart', class: '', belongsTo: 'statistics' },
+  { path: '/deathrates', title: 'Death Rates',  icon:'trending_up', class: '', belongsTo: 'statistics' },
   { path: '/economics', title: 'Economic Stimulus', hide: true, icon:'monetization_on', class: '' },
-  { path: '/surveillance', title: 'Surveillance',  icon:'policy', class: '' },
-  { path: '/borders', title: 'Border Control',  icon:'location_on', class: '' },
-  { path: '/masks', title: 'Masks', soon: true, hide: true, icon:'healing', class: '' },
+  { path: '/surveillance', title: 'Surveillance',  icon:'policy', class: '',  belongsTo: 'measures' },
+  { path: '/borders', title: 'Border Control',  icon:'location_on', class: '',  belongsTo: 'measures'},
+  { path: '/masks', title: 'Masks', soon: true, hide: true, icon:'healing', class: '',  belongsTo: 'measures' },
   { path: '/testing', title: 'Testing', soon: true, icon:'local_hospital', class: '' },
   { path: '/aboutus', title: 'About Us', icon:'supervisor_account', class: '' },
 ];
@@ -35,7 +36,16 @@ export class SidebarComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
-    this.menuItems = ROUTES.filter(menuItem => menuItem);
+    const statistics = { title: "Statistics", collapse: true, icon: 'arrow_drop_down', menus : []};
+    statistics.menus = ROUTES.filter(item => item.belongsTo == 'statistics');
+    
+    const measures =  { title: "Measures", collapse:true, icon: 'arrow_drop_down', menus : []};
+    measures.menus = ROUTES.filter(item => item.belongsTo == 'measures');
+
+    this.menuItems = ROUTES.filter(menuItem => menuItem.belongsTo == undefined);
+    this.menuItems.splice(1,0, measures);
+    this.menuItems.splice(2,0, statistics);
+
   }
   isMobileMenu() {
       if ($(window).width() > 991) {
