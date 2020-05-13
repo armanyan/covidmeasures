d9a9fc1dc456189a05ad87d211ed1c5678fe1bbf
Arman Davidyan <arman.davidyan@clearroad.io>
2020-05-13 15:34:09 +0200

    perf: change impacts format

--- src/app/impacts/impacts.component.ts
+++ src/app/impacts/impacts.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Title } from "@angular/platform-browser";
 
-import { mobileWidth } from '../utils';
+import { aws, mobileWidth } from '../utils';
 
 export interface Impact {
   impact: string,
@@ -20,7 +20,7 @@ export interface Impact {
 })
 export class ImpactsComponent implements OnInit {
   public isMobile: boolean;
-  public impactHeaders = ['Impact', 'Description', 'Location', 'Measure', 'Source'];
+  public impactHeaders = ['Location', 'Impact', 'Description', 'Measure', 'Source'];
   public impacts: Impact[] = [];
 
   constructor(
@@ -32,19 +32,8 @@ export class ImpactsComponent implements OnInit {
     this.titleService.setTitle('Impacts: COVID-19 Impacts Worldwide');
     this.isMobile = window.innerWidth > mobileWidth ? false : true;
 
-    const url = 'https://covidmeasures-data.s3.amazonaws.com/new_impacts.json';
-    const data = (await this.http.get(url).toPromise() as any);
-    for (const entry of data) {
-      const impact = (entry as any);
-      this.impacts.push({
-        "impact": impact.impact,
-        "description": impact.description,
-        "location": impact.location,
-        "measure": impact.measure,
-        "source": impact.source,
-        "link": impact.link
-      });
-    }
+    const url = `${aws}/impacts.json`;
+    this.impacts = (await this.http.get(url).toPromise() as any);
   }
 
 }
