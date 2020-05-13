e00558d98a953ba7d65f88c9f61e200ca52a57a3
eevan7a9 <e7a9@protonmail.com>
2020-05-14 01:07:42 +0900

    added testing in collapse

--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -21,7 +21,7 @@ export const ROUTES: RouteInfo[] = [
   { path: '/surveillance', title: 'Surveillance',  icon:'policy', class: '',  belongsTo: 'measures' },
   { path: '/borders', title: 'Border Control',  icon:'location_on', class: '',  belongsTo: 'measures'},
   { path: '/masks', title: 'Masks', soon: true, hide: true, icon:'healing', class: '',  belongsTo: 'measures' },
-  { path: '/testing', title: 'Testing', soon: true, icon:'local_hospital', class: '' },
+  { path: '/testing', title: 'Testing', soon: true, icon:'local_hospital', class: '', belongsTo: 'measures' },
   { path: '/aboutus', title: 'About Us', icon:'supervisor_account', class: '' },
 ];
 
