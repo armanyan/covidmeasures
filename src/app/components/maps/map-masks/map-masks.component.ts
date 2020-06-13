d147313cc159212fd6a2cd59690d55ddf30ecdba
eevan7a9 <e7a9@protonmail.com>
2020-06-14 08:38:38 +0900

    added map for masks

---
+++ src/app/components/maps/map-masks/map-masks.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-map-masks',
+  templateUrl: './map-masks.component.html',
+  styleUrls: ['./map-masks.component.css']
+})
+export class MapMasksComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
