a9d7a1bdbdc8e81ea38516b2b0fa2920998dfef7
Arman Davidyan <arman.davidyan@clearroad.io>
2020-06-24 18:24:21 +0200

    fix(titles): little trick to force title update

--- src/app/layouts/admin-layout/admin-layout.component.ts
+++ src/app/layouts/admin-layout/admin-layout.component.ts
@@ -1,8 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { Location } from '@angular/common';
-import 'rxjs/add/operator/filter';
-import { Subscription } from 'rxjs/Subscription';
 import PerfectScrollbar from 'perfect-scrollbar';
 import { Title } from '@angular/platform-browser';
 
@@ -12,9 +10,6 @@ import { Title } from '@angular/platform-browser';
   styleUrls: ['./admin-layout.component.scss']
 })
 export class AdminLayoutComponent implements OnInit {
-  private _router: Subscription;
-  private lastPoppedUrl: string;
-  private yScrollStack: number[] = [];
   public title: string; /** this will hold the title of the document */
   public isMobile: boolean;
 
@@ -25,24 +20,17 @@ export class AdminLayoutComponent implements OnInit {
   ) {}
 
   ngOnInit() {
-      
+    setInterval(() => Date.now(), 500);
   }
+
   ngAfterViewInit() {
       this.runOnRouteChange();
   }
+
   ngAfterContentChecked(){
     this.title = this.titleService.getTitle().split(':')[0]
   }
-  isMaps(path){
-      var titlee = this.location.prepareExternalUrl(this.location.path());
-      titlee = titlee.slice( 1 );
-      if(path == titlee){
-          return false;
-      }
-      else {
-          return true;
-      }
-  }
+
   runOnRouteChange(): void {
     if (window.matchMedia(`(min-width: 960px)`).matches && !this.isMac()) {
       const elemMainPanel = <HTMLElement>document.querySelector('.main-panel');
@@ -50,13 +38,11 @@ export class AdminLayoutComponent implements OnInit {
       ps.update();
     }
   }
+
   isMac(): boolean {
-      let bool = false;
-      if (navigator.platform.toUpperCase().indexOf('MAC') >= 0 || navigator.platform.toUpperCase().indexOf('IPAD') >= 0) {
-          bool = true;
-      }
-      return bool;
+    return (navigator.platform.toUpperCase().indexOf('MAC') >= 0 || navigator.platform.toUpperCase().indexOf('IPAD') >= 0);
   }
+
   isHomePage() {
     return this.router.url === '/home';
   }
