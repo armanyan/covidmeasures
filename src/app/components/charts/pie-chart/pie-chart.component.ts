06634502a82942f5b7ab3fcadb08ab08e8961f09
eevan7a9 <e7a9@protonmail.com>
2020-06-26 16:53:42 +0900

    added pie chart for q3

--- src/app/components/charts/pie-chart/pie-chart.component.ts
+++ src/app/components/charts/pie-chart/pie-chart.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Chart } from 'chart.js';
 
 @Component({
   selector: 'app-pie-chart',
@@ -7,9 +8,46 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PieChartComponent implements OnInit {
 
-  constructor() { }
+    public pieChart: Chart;
+    // REQUIRED
+    @Input() data: Array<number> = []; // holds data to be display
+    @Input() labels: Array<string> = []; // labels for data
+    // OPTIONAL
+    @Input() backgroundColor?: Array<string>; // background color for pie
+    @Input() labelFontColor?: string = '#333';
+    @Input() responsive?: boolean = true; // responsivness
+    @Input() maintainAspectRatio?: boolean = true; // aspect ratio
+    @Input() legendDisplay?: boolean = true; 
+    @Input() lagendPosition?: string = 'top';
+    @Input() legendAlign?: string = 'center';
 
-  ngOnInit(): void {
-  }
+    constructor() { }
 
+    ngOnInit(): void {
+        const ctx = (document.getElementById('pie_chart') as any).getContext('2d');
+        this.pieChart = new Chart(ctx, {
+            type: 'pie',
+            data: {
+                datasets: [{
+                    backgroundColor: this.backgroundColor,
+                    data: this.data
+                }],
+                // These labels appear in the legend and in the tooltips when hovering different arcs
+                labels: this.labels
+            },
+            options: {
+                responsive: this.responsive,
+                maintainAspectRatio: this.maintainAspectRatio,
+                scales: {},
+                legend: {
+                    display: this.legendDisplay,
+                    position: this.lagendPosition,
+                    align: this.legendAlign,
+                    labels: {
+                        fontColor: this.labelFontColor
+                    }
+                }
+            }
+        });
+    }
 }
--- src/app/masks/masks.component.ts
+++ src/app/masks/masks.component.ts
@@ -13,6 +13,11 @@ export class MasksComponent implements OnInit {
   public readMore = false;
   public countryMasksData: any;
 
+  public survey_answers: any;
+  public answers_label: Array<string>;
+  public answers_value: Array<number>;
+  public backgroundColor: Array<string> = ['rgb(91, 86, 237)','rgb(237, 86, 88)','rgb(108, 185, 113)','rgb(229, 229, 229)']
+
   constructor(
     private titleService: Title,
     private changeDetector: ChangeDetectorRef
@@ -26,10 +31,28 @@ export class MasksComponent implements OnInit {
 
   async ngAfterViewInit(){
     // this.countryMasksData = await this.http.get(`${aws}/masks_survey.json`).toPromise();
-    fetch(`${aws}/masks_survey.json`).then(res => res.json()).then(data => {
-      this.countryMasksData = data
-      this.changeDetector.detectChanges()
-    })
+    fetch(`${aws}/masks_survey.json`).then(res => res.json()).then((data) => {
+      this.countryMasksData = data;
+      this.changeDetector.detectChanges();
+      const q3Count = {};
+      data.forEach((survey) => {
+        if (survey['Q3 - Efficient']) {
+          if(q3Count[survey['Q3 - Efficient']]){
+            q3Count[survey['Q3 - Efficient']].number = q3Count[survey['Q3 - Efficient']].number + 1;
+            q3Count[survey['Q3 - Efficient']].percent = 
+              parseFloat(((q3Count[survey['Q3 - Efficient']].number / data.length) * 100).toFixed(2));
+          }else {
+            q3Count[survey['Q3 - Efficient']] = {
+              answer: survey['Q3 - Efficient'],
+              number: 1,
+              percent: parseFloat(((1 / data.length) * 100).toFixed(2))
+            };
+          }
+        }
+      });
+      this.survey_answers = Object.keys( q3Count ).map( key => q3Count[key]);
+      this.answers_label =  Object.keys( q3Count ).map( key => q3Count[key]).map( key => key.answer );
+      this.answers_value = Object.keys( q3Count ).map( key => q3Count[key]).map( key => key.percent );
+    });
   }
-
 }
