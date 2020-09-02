65e1412989760dc614d520bf669feb6e5d308a5e
eevan7a9 <e7a9@protonmail.com>
2020-09-02 14:34:49 +0900

    add to get query-params and scroll into

--- src/app/components/page-navigator/page-navigator.component.ts
+++ src/app/components/page-navigator/page-navigator.component.ts
@@ -1,5 +1,6 @@
 import { Component, HostListener, Inject, Input, OnInit } from "@angular/core";
 import { DOCUMENT } from "@angular/common";
+import { ActivatedRoute } from "@angular/router";
 @Component({
   selector: "app-page-navigator",
   templateUrl: "./page-navigator.component.html",
@@ -8,8 +9,9 @@ import { DOCUMENT } from "@angular/common";
 export class PageNavigatorComponent implements OnInit {
   @Input() pageSections: Array<{ name: string; sectionID: string }>;
   @Input() navTopDistance: number = 0;
+  private paramSection: string;
 
-  constructor(@Inject(DOCUMENT) document) {}
+  constructor(@Inject(DOCUMENT) document, private route: ActivatedRoute) {}
 
   ngOnInit(): void {
     // we get the distance of pagenave from the top of the screen
@@ -18,6 +20,14 @@ export class PageNavigatorComponent implements OnInit {
         .getElementById("pageNavbar")
         .getBoundingClientRect().top;
     }
+    // we get the params in he url
+    this.route.queryParams.subscribe(params => {
+      this.paramSection = params['section'];
+      // we add little delay
+      setTimeout(()=> {
+        this.scrollInto(this.paramSection)
+      }, 1000)
+    });
   }
 
   @HostListener("window:scroll", ["$event"])
