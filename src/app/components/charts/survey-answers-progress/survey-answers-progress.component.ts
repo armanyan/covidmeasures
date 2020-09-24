077014c51d6b76eacdf00bb9b0f64d37158f27f9
eevan7a9 <e7a9@protonmail.com>
2020-09-24 23:25:02 +0900

    develop survey-answers-progress component

---
+++ src/app/components/charts/survey-answers-progress/survey-answers-progress.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-survey-answers-progress',
+  templateUrl: './survey-answers-progress.component.html',
+  styleUrls: ['./survey-answers-progress.component.css']
+})
+export class SurveyAnswersProgressComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
