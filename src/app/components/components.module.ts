145e60bac8698483ab917b4673394052771fc724
eevan7a9 <e7a9@protonmail.com>
2020-09-24 23:09:34 +0900

    add custom modules to the export of ComponentModules

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -6,6 +6,18 @@ import { FooterComponent } from './footer/footer.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { EconomicDataComponent } from './economic-data/economic-data.component';
+import { MapSchoolClosureComponent } from './maps/map-school-closure/map-school-closure.component';
+import { MapLockdownComponent } from './maps/map-lockdown/map-lockdown.component';
+import { MapMasksComponent } from './maps/map-masks/map-masks.component';
+import { DisqusCommentsComponent } from './pages-comments/disqus-comments.component';
+
+import { PageLoadersComponent } from './page-loaders/page-loaders.component';
+import { RemindMeComponent } from './remind-me/remind-me.component';
+import { EvolutionCountryComponent } from './charts/evolution-country/evolution-country.component';
+import { PageQuestionComponent } from './page-question/page-question.component'
+import { PageNavigatorComponent } from './page-navigator/page-navigator.component';
+import { PageSeparatorComponent } from './page-separator/page-separator.component';
+import { SurveyAnswersProgressComponent } from './charts/survey-answers-progress/survey-answers-progress.component';
 
 @NgModule({
   imports: [
@@ -16,12 +28,34 @@ import { EconomicDataComponent } from './economic-data/economic-data.component';
     FooterComponent,
     NavbarComponent,
     SidebarComponent,
-    EconomicDataComponent
+    EconomicDataComponent,
+    SurveyAnswersProgressComponent,
+    MapSchoolClosureComponent,
+    MapLockdownComponent,
+    MapMasksComponent,
+    DisqusCommentsComponent,
+    PageLoadersComponent,
+    RemindMeComponent,
+    EvolutionCountryComponent,
+    PageQuestionComponent,
+    PageNavigatorComponent,
+    PageSeparatorComponent,
   ],
   exports: [
     FooterComponent,
     NavbarComponent,
-    SidebarComponent
+    SidebarComponent,
+    SurveyAnswersProgressComponent,
+    MapSchoolClosureComponent,
+    MapLockdownComponent,
+    MapMasksComponent,
+    DisqusCommentsComponent,
+    PageLoadersComponent,
+    RemindMeComponent,
+    EvolutionCountryComponent,
+    PageQuestionComponent,
+    PageNavigatorComponent,
+    PageSeparatorComponent,
   ]
 })
 export class ComponentsModule { }
