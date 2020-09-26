1c5180fd7b9de42f1ef5cd95126813439c12ab3d
eevan7a9 <e7a9@protonmail.com>
2020-09-26 20:43:58 +0900

    get the number of answers per question

--- src/app/components/charts/survey-answers-progress/survey-answers-progress.component.ts
+++ src/app/components/charts/survey-answers-progress/survey-answers-progress.component.ts
@@ -1,15 +1,44 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { aws } from "../../../utils";
 
 @Component({
-  selector: 'app-survey-answers-progress',
-  templateUrl: './survey-answers-progress.component.html',
-  styleUrls: ['./survey-answers-progress.component.css']
+  selector: "app-survey-answers-progress",
+  templateUrl: "./survey-answers-progress.component.html",
+  styleUrls: ["./survey-answers-progress.component.css"],
 })
 export class SurveyAnswersProgressComponent implements OnInit {
-
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
-  }
+    fetch(`${aws}/community_school_reopening.json`)
+      .then((res) => res.json())
+      .then((data: Object[]) => {
+        const surveyResults = {};
 
+        console.log("progress");
+        data.forEach((data) => {
+          for (const key in data) {
+            if (Object.prototype.hasOwnProperty.call(data, key)) {
+              const answer = data[key];
+              const attr = key.split(" ")[0];
+
+              if (surveyResults[attr]) {
+                if (surveyResults[attr][answer]) {
+                  surveyResults[attr][answer] = surveyResults[attr][answer] + 1;
+                  console.log("if", surveyResults[attr]);
+                } else {
+                  surveyResults[attr][answer] = 1;
+                }
+              } else {
+                surveyResults[attr] = {
+                  [answer]: 1,
+                };
+                console.log("else", surveyResults[attr]);
+              }
+            }
+          }
+        });
+        console.log(surveyResults);
+      });
+  }
 }
