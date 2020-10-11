8f38457088b1497f5c74ac030e8de2d01cccaabd
eevan7a9 <e7a9@protonmail.com>
2020-10-11 21:56:39 +0900

    added survey gatherings answer component

---
+++ src/app/components/charts/survey-gatherings-answers/survey-gatherings-answers.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-survey-gatherings-answers',
+  templateUrl: './survey-gatherings-answers.component.html',
+  styleUrls: ['./survey-gatherings-answers.component.css']
+})
+export class SurveyGatheringsAnswersComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
